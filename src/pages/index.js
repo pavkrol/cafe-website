150745b70aed01de15975f84ffd26cfc9bc46095
pavkrol <pdkrol@gmail.com>
2019-11-07 15:49:23 +0100

    changed imports order to fix problem with css inconsistency

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
+import { theme } from "../components/theme"
+import { ThemeProvider } from "styled-components"
 import Layout from "../components/layout"
 import Hero from "../containers/Hero"
 import SEO from "../components/seo"
 import ProductsSection from "../containers/ProductsSection"
 import BlogSection from "../containers/BlogSection"
-import { theme } from "../components/theme"
-import { ThemeProvider } from "styled-components"
 
 const IndexPage = () => (
   <ThemeProvider theme={theme}>

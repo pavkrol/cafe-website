85010fe34d7845042820afbd8153a1401221dc81
pavkrol <pdkrol@gmail.com>
2019-11-07 14:29:15 +0100

    change import paths

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import GalleryView from "../containers/GalleryView"
-import SEO from "../components/Seo"
+import SEO from "../components/seo"
 import { theme } from "../components/theme"
 import { ThemeProvider } from "styled-components"
 
